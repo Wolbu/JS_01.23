c8f5642d7e67b0a3d3c36914dc3ce652eb66a35b
Wolbu <94459033+Wolbu@users.noreply.github.com>
2023-02-10 18:27:43 +0100

    [4.2:10.02]: finished second task

--- index.js
+++ index.js
@@ -11,3 +11,48 @@ function curry(fn) {
     }
   };
 }
+
+//! Task 2
+
+class Calculator {
+  constructor(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+      throw new Error("Both arguments must be numbers");
+    }
+    this._x = x;
+    this._y = y;
+  }
+
+  setX(x) {
+    if (typeof x !== "number") {
+      throw new Error("X must be a number");
+    }
+    this._x = x;
+  }
+
+  setY(y) {
+    if (typeof y !== "number") {
+      throw new Error("Y must be a number");
+    }
+    this._y = y;
+  }
+
+  getSum() {
+    return this._x + this._y;
+  }
+
+  getMul() {
+    return this._x * this._y;
+  }
+
+  getSub() {
+    return this._x - this._y;
+  }
+
+  getDiv() {
+    if (this._y === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return this._x / this._y;
+  }
+}
