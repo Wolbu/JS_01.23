5ca50f9cfea8854c61713279822cdb141efef432
Wolbu <94459033+Wolbu@users.noreply.github.com>
2023-02-15 21:10:20 +0100

    [5.3:15.02]: finished third task

--- index.js
+++ index.js
@@ -110,3 +110,135 @@ class LinkedList {
     return list;
   }
 }
+
+//! Task 3
+
+class Car {
+  #brand = "";
+  #model = "";
+  #yearOfManufacturing = 1950;
+  #maxSpeed = 100;
+  #maxFuelVolume = 20;
+  #fuelConsumption = 1;
+  #damage = 1;
+  #currentFuelVolume = 0;
+  #isStarted = false;
+  #mileage = 0;
+  #health = 100;
+
+  // Getters
+  get brand() {
+    return this.#brand;
+  }
+  get model() {
+    return this.#model;
+  }
+  get yearOfManufacturing() {
+    return this.#yearOfManufacturing;
+  }
+  get maxSpeed() {
+    return this.#maxSpeed;
+  }
+  get maxFuelVolume() {
+    return this.#maxFuelVolume;
+  }
+  get fuelConsumption() {
+    return this.#fuelConsumption;
+  }
+  get damage() {
+    return this.#damage;
+  }
+  get currentFuelVolume() {
+    return this.#currentFuelVolume;
+  }
+  get isStarted() {
+    return this.#isStarted;
+  }
+  get mileage() {
+    return this.#mileage;
+  }
+  get health() {
+    return this.#health;
+  }
+
+  // Setters
+  set brand(newBrand) {
+    if (
+      typeof newBrand === "string" &&
+      newBrand.length > 0 &&
+      newBrand.length <= 50
+    ) {
+      this.#brand = newBrand;
+    } else {
+      throw new Error("Invalid brand name");
+    }
+  }
+  set model(newModel) {
+    if (
+      typeof newModel === "string" &&
+      newModel.length > 0 &&
+      newModel.length <= 50
+    ) {
+      this.#model = newModel;
+    } else {
+      throw new Error("Invalid model name");
+    }
+  }
+  set yearOfManufacturing(newYear) {
+    const currentYear = new Date().getFullYear();
+    if (
+      typeof newYear === "number" &&
+      newYear >= 1950 &&
+      newYear <= currentYear
+    ) {
+      this.#yearOfManufacturing = newYear;
+    } else {
+      throw new Error("Invalid year of manufacturing");
+    }
+  }
+  set maxSpeed(newMaxSpeed) {
+    if (
+      typeof newMaxSpeed === "number" &&
+      newMaxSpeed >= 100 &&
+      newMaxSpeed <= 330
+    ) {
+      this.#maxSpeed = newMaxSpeed;
+    } else {
+      throw new Error("Invalid max speed");
+    }
+  }
+  set maxFuelVolume(newMaxFuelVolume) {
+    if (
+      typeof newMaxFuelVolume === "number" &&
+      newMaxFuelVolume >= 20 &&
+      newMaxFuelVolume <= 100
+    ) {
+      this.#maxFuelVolume = newMaxFuelVolume;
+    } else {
+      throw new Error("Invalid max fuel volume");
+    }
+  }
+  set fuelConsumption(newFuelConsumption) {
+    if (typeof newFuelConsumption === "number" && newFuelConsumption > 0) {
+      this.#fuelConsumption = newFuelConsumption;
+    } else {
+      throw new Error("Invalid fuel consumption");
+    }
+  }
+  set damage(newDamage) {
+    if (typeof newDamage === "number" && newDamage >= 1 && newDamage <= 5) {
+      this.#damage = newDamage;
+    } else {
+      throw new Error("Invalid damage");
+    }
+  }
+
+  // Methods
+  start() {
+    if (this.#isStarted) {
+      throw new Error("Car has already started");
+    } else {
+      this.#isStarted = true;
+    }
+  }
+}
