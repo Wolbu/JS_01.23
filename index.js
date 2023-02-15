75b21819c9202f0b0cf58594e8340c48397de20b
Wolbu <94459033+Wolbu@users.noreply.github.com>
2023-02-15 19:44:52 +0100

    [5.2:15.02]: finished second task

--- index.js
+++ index.js
@@ -47,3 +47,66 @@ class Stack {
     return stack;
   }
 }
+
+//! Task 2
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(elem) {
+    const node = { value: elem, next: null };
+    if (this.size === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  prepend(elem) {
+    const node = { value: elem, next: this.head };
+    this.head = node;
+    if (this.size === 0) {
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  find(elem) {
+    let current = this.head;
+    while (current) {
+      if (current.value === elem) {
+        return current;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+
+  toArray() {
+    const result = [];
+    let current = this.head;
+    while (current) {
+      result.push(current.value);
+      current = current.next;
+    }
+    return result;
+  }
+
+  static fromIterable(iterable) {
+    if (!iterable[Symbol.iterator]) {
+      throw new Error("Passed entity is not iterable");
+    }
+    const list = new LinkedList();
+    for (const elem of iterable) {
+      list.append(elem);
+    }
+    return list;
+  }
+}
